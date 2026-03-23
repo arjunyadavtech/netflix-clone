d5d65667c64bf9285923705f72392733fa7b79c6
Arjunnyadav <arjunnyadav01@gmail.com>
2026-03-23 14:58:36 +0530

    remove netflix identity

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const faqs = [
     {
-        question: "What is Netflix?",
-        answer: `Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.
+        question: "What is StreamX?",
+        answer: `StreamX is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.
 
         You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!`
     },
     {
-        question: "How much does Netflix cost?",
-        answer: `Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹149 to ₹649/month.`
+        question: "How much does StreamX cost?",
+        answer: `Watch StreamX on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹149 to ₹649/month.`
     },
     {
         question: "What can I watch?",
-        answer: `Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web or on any internet-connected device.
+        answer: `Watch anywhere, anytime. Sign in with your StreamX account to watch instantly on the web or on any internet-connected device.
 
         You can also download your favourite shows with the app and watch offline.`
     },
     {
         question: "How do I cancel?",
-        answer: `Netflix is flexible. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.`
+        answer: `StreamX is flexible. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.`
     },
     {
-        question: "What can I watch on Netflix?",
-        answer: `Netflix has an extensive library of feature films, documentaries, shows, anime, award-winning Netflix originals, and more.`
+        question: "What can I watch on StreamX?",
+        answer: `StreamX has an extensive library of feature films, documentaries, shows, anime, award-winning StreamX originals, and more.`
     },
     {
-        question: "Is Netflix good for kids?",
-        answer: `The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly content.
+        question: "Is StreamX good for kids?",
+        answer: `The StreamX Kids experience is included in your membership to give parents control while kids enjoy family-friendly content.
 
         Kids profiles come with PIN-protected parental controls.`
     }
